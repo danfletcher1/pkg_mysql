e05def759fcb18bd1693bbfe1002c0fb94db9dce
Dan Fletcher <danfle.ig@googlemail.com>
2022-08-02 00:42:15 +0100

    Update mysql.go

--- mysql.go
+++ mysql.go
@@ -68,34 +68,42 @@ import (
 	"errors"
 	"fmt"
 	"strings"
-
+	"context"
+	"io"
+	"bufio"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 // this is a pointer to db not a value
 type DB struct {
 	db *sql.DB
+	schemaChanges int
+	schemaFail bool
 }
 
 // Connect function opens a new database connection this must be done before reading/writing to the database
-func (d *DB) Connect(username, password, database, host string) error {
+func Connect(username, password, database, host string) (*DB, error) {
 	// Attempt to open the database connection
-	var err error
-	d.db, err = sql.Open("mysql", username+":"+password+"@("+host+")/"+database)
+	var (
+		err error
+		this = new(DB)
+	)
+
+	this.db, err = sql.Open("mysql", username+":"+password+"@("+host+")/"+database)
 	if err != nil {
-		fmt.Println(err)
-		return errors.New("Unable to connect to DB" + err.Error())
+		return nil, errors.New("Unable to connect to DB" + err.Error())
 	}
 
 	// Open doesn't open a connection. Validate DSN data:
-	err = d.db.Ping()
+	err = this.db.Ping()
 	if err != nil {
-		return errors.New("Unable to validate DB:DSN" + err.Error())
+		this.db.Close()
+		return nil, errors.New("Unable to validate DB:DSN" + err.Error())
 	}
 
-	d.db.SetMaxIdleConns(20)
-	d.db.SetMaxOpenConns(20)
-	return nil
+	this.db.SetMaxIdleConns(20)
+	this.db.SetMaxOpenConns(20)
+	return this, nil
 }
 
 // Close should executed right after connect with the defer keyword
@@ -103,9 +111,160 @@ func (d *DB) Close() {
 	d.db.Close()
 }
 
-// func (d *DB) Conn() (*db.Conn, error) {
-// 	return d.db.Conn()
-// }
+
+/* 
+  We can now manage schemea changes. 
+  Consideration has been paid to prevent changes out of sequence. 
+  The exact order is recorded in the DB, any future updates that do not obey that strict order, block any updates. 
+  Existing packages did nothing to prevent you applying updates that only partly execute.
+  Existing packages did not prevent multiple developers making schema changes that conflict
+  Existing packages did nothing to endure identical schemea is identical on all production and lab deployments.
+*/
+func (d *DB) Schema(ctx context.Context, schema io.Reader) error {
+	
+	var currentTx string
+	var data []byte
+	
+	// Because CREATE has an implied commit, we will create the schema DB seporately. 
+	tx, e := d.db.BeginTx(ctx, nil)
+	if e != nil {
+		return fmt.Errorf("unable to start a new transaction: %v", e)
+	}
+
+	if _, e = tx.Exec(`CREATE TABLE IF NOT EXISTS dbSchemaLog (id int(11) NOT NULL, tx TEXT, lastModify timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(), PRIMARY KEY (id))`); e != nil {
+		return fmt.Errorf("unable to create dbSchemaLog: %v", e)
+	}
+	
+	e = tx.Commit()
+	if e != nil {
+		return fmt.Errorf("unable to commit dbSchemaLog: %v", e)
+	}
+
+	schemaData := bufio.NewReader(schema)
+	for {
+		// process each line
+		b, o, e := schemaData.ReadLine()
+
+		if o {
+			data = append(data, b...)
+			continue
+		}
+		if len(data) > 0 {
+			b = append(data, b...)
+			data = []byte{}
+		}
+		if e == io.EOF {
+			return nil
+		}
+		if e != nil {
+			return fmt.Errorf("error reading scheme lines, %v", e)
+		}
+		if len(b) == 0 {
+			continue
+		}
+	
+		d.schemaChanges++
+
+		// Check the sequence of instructions matches our exact history
+		e = d.db.QueryRowContext(ctx, "SELECT tx FROM dbSchemaLog WHERE id=?", d.schemaChanges).Scan(&currentTx)
+		// if we have an exact match, great skip to the next
+		if currentTx == string(b) {
+			continue
+		}
+		// We expect an error of ErrNoResult, anything else is bad
+		if e != sql.ErrNoRows {
+			return fmt.Errorf("dbSchemaLog does not match schema being applied at row %v. dbScheamLog has %v, schema applied %v", d.schemaChanges, currentTx, string(b))
+		}
+
+		// Start a transaction
+		tx, e = d.db.BeginTx(ctx, nil)
+		if e != nil {
+			return fmt.Errorf("unable to start a new transaction: %v", e)
+		}
+
+		// Insert into the dbSchemaLog
+		_, e = tx.ExecContext(ctx, "INSERT INTO dbSchemaLog (id, tx) VALUES (?, ?)", d.schemaChanges, string(b))
+		if e != nil {
+			tx.Rollback()
+			return fmt.Errorf("unable to insert dbSchemaLog row %v, error was %v", d.schemaChanges, e)
+		}
+
+		// Make the schema change (usually an implied commit)
+		_, e = tx.ExecContext(ctx, string(b))
+		if e != nil {
+			// Because of implied commit it doesn't remove the DB schema entry
+			tx.ExecContext(ctx, "DELETE FROM dbSchemaLog WHERE id=?", d.schemaChanges)
+			tx.Commit()
+			return fmt.Errorf("unable to make schema change row %v: %v: %v",d.schemaChanges, string(b), e)
+		}
+
+		// No errors commit if we haven't already
+		e = tx.Commit()
+		if e != nil {
+			tx.Rollback()
+			return fmt.Errorf("unable to commit schema change row %v: %v: %v", d.schemaChanges, string(b), e)
+		}
+		
+	}
+	return nil
+}
+
+
+/* 
+***** Statements that cause an implicit commit ****
+*****        Ordering can help here            ****
+ALTER DATABASE ... UPGRADE DATA DIRECTORY NAME
+ALTER EVENT
+ALTER FUNCTION
+ALTER PROCEDURE
+ALTER SERVER
+ALTER TABLE
+ALTER VIEW
+ANALYZE TABLE
+BEGIN
+CACHE INDEX
+CHANGE MASTER TO
+CHECK TABLE
+CREATE DATABASE
+CREATE EVENT
+CREATE FUNCTION
+CREATE INDEX
+CREATE PROCEDURE
+CREATE ROLE
+CREATE SERVER
+CREATE TABLE
+CREATE TRIGGER
+CREATE USER
+CREATE VIEW
+DROP DATABASE
+DROP EVENT
+DROP FUNCTION
+DROP INDEX
+DROP PROCEDURE
+DROP ROLE
+DROP SERVER
+DROP TABLE
+DROP TRIGGER
+DROP USER
+DROP VIEW
+FLUSH
+GRANT
+LOAD INDEX INTO CACHE
+LOCK TABLES
+OPTIMIZE TABLE
+RENAME TABLE
+RENAME USER
+REPAIR TABLE
+RESET
+REVOKE
+SET PASSWORD
+SHUTDOWN
+START SLAVE
+START TRANSACTION
+STOP SLAVE
+TRUNCATE TABLE
+*/
+
 
 // Fetch will do a select query and return as string or int
 func (d *DB) Fetch(where map[string]interface{}, table string) (map[int]map[string]interface{}, error) {
@@ -309,7 +468,7 @@ func (d *DB) Update(updates map[string]interface{}, table string) (count int64, 
 	vals[i] = id
 
 	if id == "" {
-		return 0, errors.New("Row [" + string(i) + "] is missing the ID, please always include a row ID column")
+		return 0, fmt.Errorf("Row [%v] is missing the ID, please always include a row ID column", i)
 	}
 
 	// execute the statement with vals values
@@ -352,3 +511,29 @@ func (d *DB) Delete(updates map[string]interface{}, table string) (count int64, 
 
 	return rowCnt, nil
 }
+
+
+/*
+	Developing a new range of local table caching functions
+	
+	Each table requires a lastmod and index
+
+	In the event of a conflict, the most recent change overwrites
+
+	Works best if only one system is modifying the data
+
+	Multiple read replicas are fine
+
+	Its essential the local table continues to function if the DB is unavailable. 
+*/
+
+
+// func (d *DB) CreateCacheTable() *CachedTable, error {}
+
+// func (c *CachedTable) Sync() error {}
+
+
+
+// type CachedTable struct {
+
+// }
